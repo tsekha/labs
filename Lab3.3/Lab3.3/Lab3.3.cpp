92f831f48fe67a5ed930420f7b1df625cd337b83
tsekha <tsekhanovich06@mail.ru>
2023-12-02 18:43:17 +0300

    Lab3.3 added

---
+++ Lab3.3/Lab3.3/Lab3.3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+int main()
+{
+    int n;
+    std::cout << "N= ";
+    std::cin >> n;
+
+    bool* arr = new bool[n + 1];
+    for (int i = 2; i < n + 1; i++)
+    {
+        arr[i] = true;
+    }
+
+    for (int i = 2; i < n + 1; i++)
+    {
+        if (arr[i])
+        {
+            for (int k = i * i; k < n + 1; k += i)
+            {
+                arr[k] = false;
+            }
+        }
+    }
+
+    for (int i = n; i >= 0; i--)
+    {
+        if (arr[i])
+        {
+            std::cout << i;
+            break;
+        }
+    }
+}
